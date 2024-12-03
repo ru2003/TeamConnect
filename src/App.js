7152a885fc76b980b8bb53c5c9a03bb83a1b2bfe
ru2003 <ruchitanchaudhari145@gmail.com>
2024-12-03 12:24:22 +0530

    done some changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-
+import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
+    //BEM naming convention for the defaultS
     <div className="App">
       <h1>
-        Hello Programmers...lets buid the TeamConnect :rocket:
-    
+        Hello Programmers...lets buid the TeamConnect 
       </h1>
+      
+      {/*Header */}
+      <Header/>
+
+      
+       {/*Sidebar */}
+        {/*react-Router -> */}
+
+
+
     </div>
   );
 }
---
+++ src/Header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import './Header.css';
+
+function Header() {
+  return (
+    <div className="header"> 
+    <div className='header_left'>
+        {/* Avtars for logged in user   */}
+        {/* Time icon */}
+   </div>
+
+   <div className='header_search' >
+    {/* Search icon */}
+    {/* input*/}
+   </div>
+
+   <div className='header_right'>
+    {/* help icon */}
+
+   </div>
+
+
+
+
+    </div>
+  )  
+}
+
+
+export default Header
