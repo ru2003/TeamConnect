11fab5f76901f03021bfb3141eedbc798d08909f
ru2003 <ruchitanchaudhari145@gmail.com>
2024-12-30 14:38:30 +0530

    new

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import"./Sidebar.css";
+import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord"
+import CreateIcon from "@mui/icons-material/Create";
+
+
 
 function Sidebar() {
   return (
     <div className='sidebar'>
+        <div className="sidebar_header">
+            <div className="sidebar_info">
+            <h2>Clever Programmers </h2>
+             <h3>
+                <FiberManualRecordIcon/>
+                Ruchita Ch.
+             </h3>
+             </div>
+             <CreateIcon />
+        </div>
      
 
 
