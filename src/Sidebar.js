d4982857403c36ffc8f7e911dea3666eeeb0a9cb
ru2003 <ruchitanchaudhari145@gmail.com>
2024-12-31 22:27:44 +0530

    new

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -4,6 +4,14 @@ import FiberManualRecordIcon from "@mui/icons-material/FiberManualRecord"
 import CreateIcon from "@mui/icons-material/Create";
 import InsertCommentIcon from "@mui/icons-material/InsertComment";
 import SidebarOption from './SidebarOption';
+import InboxIcon from "@mui/icons-material/Inbox";
+import DraftsIcon from "@mui/icons-material/Drafts";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+import FileCopyIcon from "@mui/icons-material/FileCopy";
+import PeopleAltIcon from "@mui/icons-material/PeopleAlt";
+import AppsIcon from "@mui/icons-material/Apps";
+import ExpandLessIcon from "@mui/icons-material/ExpandLess";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
 
 function Sidebar() {
   return (
@@ -19,14 +27,21 @@ function Sidebar() {
              <CreateIcon />
              </div>
              
-             <SidebarOption Icon={InsertCommentIcon} title="Threads"/>
-             <SidebarOption title="youtube"/>
-
-
-        </div>
-     
-
-
+             <SidebarOption Icon={InsertCommentIcon} title="Threads"/>  
+             <SidebarOption Icon={InboxIcon} title="Mentions & reactions"/> 
+             <SidebarOption Icon={DraftsIcon} title="Saved items"/> 
+             <SidebarOption Icon={BookmarkBorderIcon} title="Channel browser"/>  
+             <SidebarOption Icon={PeopleAltIcon} title="People & user groups"/>  
+             <SidebarOption Icon={AppsIcon} title="Apps"/>  
+             <SidebarOption Icon={FileCopyIcon} title="File browser"/>  
+               
+             <SidebarOption Icon={ExpandLessIcon} title="Show less"/>
+             <hr/>
+             <SidebarOption Icon={ExpandMoreIcon} title="Channels"/>
+             <hr/>
+             </div>
+             /*  Connect to dB and list all the channels */
+            /* SidebarOption ....*/
   
   )
 }
