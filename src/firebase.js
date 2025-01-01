5a57c3168a33a19ff857af7555aa2b4b1dfd4f2c
ru2003 <ruchitanchaudhari145@gmail.com>
2025-01-01 13:09:33 +0530

    new

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyB06Vw_jMlRsfFx1a3WOOb6htDbN2CjWXo",
+    authDomain: "teamconnect-d53fa.firebaseapp.com",
+    projectId: "teamconnect-d53fa",
+    storageBucket: "teamconnect-d53fa.firebasestorage.app",
+    messagingSenderId: "694131300275",
+    appId: "1:694131300275:web:fe87cae1ee8b5645909dc6",
+    measurementId: "G-LYCFGMRG09"
+  };
+  
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
